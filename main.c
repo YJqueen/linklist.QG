f61fcc365a371f8e98bcf86575a82791c6f2f612
YJqueen <48873847+YJqueen@users.noreply.github.com>
2019-03-24 22:01:27 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,170 +1,136 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Student
-		{
-			long num;
-			float score;
-			struct Student *next;
-		}LinkList;
 
-//修改链表节点值
+typedef struct DoubleLinkNode
 
-void change(LinkList *list,int n)       //n为第n个节点
 {
-	int i = 0;
-	while (i < n && list != NULL)
+
+   int data;
+
+   struct DoubleLinkNode *prev;
+
+   struct DoubleLinkNode *next;
+
+}Node;
+
+
+void print(Node*head)
+{
+	Node*temp;
+	int j = 0;
+	temp = head;
+	while (temp->next != NULL)
 	{
-		list = list->next;
-		i++;
-	}
-	if (list != NULL)
-	{
-		puts("input a change number:");
-		scanf("%ld,%f",&list->num, &list->score);
-	}
-	else
-	{
-		puts("the node does not exist.");
+		j++;
+		printf("%d ",temp->data);
 	}
 }
 
-	//删除链表节点
+//插入节点
 
-	void delet(LinkList *list,int n)    //n为要删除的节点
-	{
-		int i = 0;
-		while (i < n && list != NULL)
-		{
-			list = list->next;
-			i++;
-		}
-		if (list != NULL)
-		{
-			list = list->next->next;
-			free(list->next);
-		}
-		else
-		{
-			puts("the node does not exist.");
-		}
-	}
-
-	//插入链表节点
-
-	void insert(LinkList *list, int n)
-	{
-		LinkList *in;
-		int i = 0;
-		while (i < n && list != NULL)
-		{
-			list = list->next;
-			i++;
-		}
-		if (list != NULL)
-		{
-			in = (LinkList*)malloc(sizeof(LinkList));
-			puts("input the insert number:");
-			scanf("%ld,%f",&in->num, &in->score);
-			in->next = list->next;   //把in的指针域指向list的下一个节点
-			list->next = in;         //把list的指针域指向in
-		}
-		else
-		{
-			puts("the node does not exist.");
-		}
-	}
-
-
-	//清空链表
-
-	void EmptyTheLinkList(LinkList *list)
-	{
-		LinkList* psave; // 用来保存当前清除项的下一节点的地址
-		while (list != NULL)
-		{
-			psave = list->next;
-			free(list);
-			list = psave;
-		}
-	}
-
-
-	//输出链表
-
-	void output(LinkList *list)
-	{
-		while (list->next != NULL)
-		{
-			list = list->next;
-			printf("\nnum:%ld\nscore:%f\n",list->num, list->score);
-		}
-	}
-
-	struct Student *stu(void)
+int InsertNode(Node*head)
 {
+	Node *new1;
+	Node *pnext = head;
+	int i = 0;
 	int n;
-	struct Student *head, *p1, *p2;
-	n = 0;
-	p1 = p2 =(struct Student*)malloc(sizeof(struct Student));
-	scanf("%ld,%f",&p1->num,&p1->score);
-	head = NULL;
-	while (p1->num != 0)
+	printf("请输入插入第几个节点:\n");
+	scanf("%d",&n);
+	while ((i < n) && (pnext != NULL))
 	{
-		n++;
-		if (n == 1)
-		{
-			head = p1;
-		}
-		else
-		{
-			p2->next = p1;
-		}
-		p2 = p1;
-		p1 = (struct Student*)malloc(sizeof(struct Student));
-		scanf("%ld,%f",&p1->num,&p1->score);
+		i++;
+		pnext = pnext->next;
 	}
-	p2->next = NULL;
-	return (head);
+	if (pnext == NULL)
+	{
+		return 1;
+	}
+	else
+	{
+		new1 = (Node*)malloc(sizeof(Node));
+		printf("请输入插入节点数据:");
+		scanf("%d",&new1->data);
+
+		new1->next = pnext->next;
+		if (n == 0)
+		{
+			pnext->next = new1;
+			pnext->next->prev = new1;
+			new1->prev = pnext;
+		}
+
+	}
+	return 0;
+}
+
+//删除节点
+
+int DeleteNode(Node*head)
+{
+	Node *pnext = head;
+	Node *ptr;
+	int n;
+	int i = 0;
+	printf("要删除那个节点?");
+	scanf("%d",&n);
+	while ((i < n) && (pnext != NULL))
+	{
+		i++;
+		pnext = pnext->next;
+	}
+	if (pnext->next->next != NULL)
+	{
+		ptr = pnext->next;
+		ptr->next->prev = pnext;
+		pnext = ptr->next;
+		free(ptr);
+	}
+	else
+	{
+		ptr = pnext->next;
+		free(ptr);
+	}
+	return 0;
+}
+
+Node*Create_Double_link()
+	{
+		Node*head;
+		Node*pnext;
+		Node*plast;
+		int i, n;
+		head = (Node*)malloc(sizeof(Node));
+		head->prev = NULL;
+		head->next = NULL;
+
+		printf("请输入双向链表的长度:\n");
+		scanf("%d",&n);
+		while (n!=0)
+		{
+			plast = head;
+			for (i = 0; i < n; i++)
+			{
+				pnext = (Node*)malloc(sizeof(Node));
+				printf("输入相应的数据：");
+				scanf("%d",&pnext->data);
+				plast->next = pnext;
+				pnext->prev = plast;
+				plast = plast->next;
+			}
+			pnext->next = NULL;
+			break;
+		}
+		return head;
 }
 
 int main()
 {
 
-		int n = 0;
-		scanf("%d",&n);
-		printf("请根据n的值执行相应过程\n");
-		printf("n = 0,修改节点；");
-		printf("n = 1,删除节点；");
-		printf("n = 2,插入节点；");
-		printf("n = 3,清空链表；");
-		printf("n = 4,输出链表；");
-		LinkList *p;
-		p = stu();
-		switch(n)
-		{
-			case 0:{int a1;
-					printf("请输入要修改第几个节点：\n");
-					scanf("%d", &a1);
-					change(p,a1);
-					break;
-				   }
-			case 1:{int a2;
-					printf("请输入要删除哪个节点：\n");
-					scanf("%d", &a2);
-					delet(p, a2);
-					break;
-				   }
-			case 2:{int a3;
-					printf("请输入要插入第几个节点:\n");
-					scanf("%d", &a3);
-					insert(p, a3);
-					break;
-				   }
-			case 3:{EmptyTheLinkList(p);break;}
-			case 4:output(p);
-					break;
-		}
-
-    return 0;
+	Node *head = Create_Double_link();
+	InsertNode(head);
+	DeleteNode(head);
+	print(head);
+	return 0;
 }
+
